59eab454332f8e8ae5aff9a79b88328888cb2916
Kamlesh P <kamlesh@assetmantle.one>
2022-10-14 08:56:04 +0530

    created new stub with different telescope approach

--- scripts/contracts.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { generate } from "cosmwasm-typescript-gen";
-import { join } from "path";
-
-generate("cw20", ["../source/schema/cw20-base"], "../src/contracts");
-
-// cli command: cosmwasm-typescript-gen generate --schema ./source/schema/cw20-base/ --out ./src/contracts --name cw20
