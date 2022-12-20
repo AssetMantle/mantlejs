8a3a63fa8cc4a2c81b8f6b62a52e1bdfbd164703
Kamlesh P <kamlesh@assetmantle.one>
2022-12-20 16:01:51 +0530

    replaced old config, new ts-babel hybrid project

--- build/helpers/helper.d.ts
+++
@@ -1,4 +0,0 @@
-import { Promise } from "es6-promise";
-export declare const checkRawLog: (log: any) => Promise<boolean>;
-export declare const getAccount: (address: string, path: string) => Promise<any>;
-export declare const FindInResponse: (type: string, list: [], id: string) => Promise<any>;
